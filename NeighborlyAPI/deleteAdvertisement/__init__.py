6af2e8f259997bf8187d6ed1136f81030c190337
anshsrtv <ansh_srtv@outlook.com>
2021-09-16 02:04:17 +0530

    Fixed Kubes & Added screenshots

--- NeighborlyAPI/deleteAdvertisement/__init__.py
+++ NeighborlyAPI/deleteAdvertisement/__init__.py
@@ -9,9 +9,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         # try:
-        url = "mongodb://neighbourlyserver:y9i5GdhXxYq8KeITUXRBJa3uug3PaZeZqBJ0uIjYxyk4LPBD2I0mpLilv3CdhvukPwLIqWw6hNQefpc4w0Wbyg%3D%3D@neighbourlyserver.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighbourlyserver@"
+        url = "mongodb://neighbourlyserver:gQAc6yevpaKB6T1KWKYdiPMXtLLZNBbRtncMxL1osx80XNzli0Isk9cnY2iiV2biKiiTdo7toJULAsx4HnmxTw%3D%3D@neighbourlyserver.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighbourlyserver@"
         client = pymongo.MongoClient(url)
-        database = client['neighbourlymongodb']
+        database = client['neighbourlydb']
         collection = database['advertisements']
         
         query = {'_id': ObjectId(id)}
